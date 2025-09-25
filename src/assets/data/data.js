9892faec45651ff4a932499ddf269b029008d793
linda2204 <yadilinda.family@gmail.com>
2025-09-25 20:12:59 +0700

    Add files via upload

---
+++ src/assets/data/data.js
@@ -0,0 +1,126 @@
+export const data = {
+    bride: {
+        L: {
+            id: 1,
+            name: 'Nuryadi',
+            child: 'Putra ke Dua',
+            father: 'Dedi Sukarta',
+            mother: 'Iroh',
+            image: './src/assets/images/cowo.png'
+        },
+        P: {
+            id: 2,
+            name: 'Linda Lidiana',
+            child: 'Putri ke Dua',
+            father: 'Nasim',
+            mother: 'Tunah',
+            image: './src/assets/images/cewe.png'
+        },
+
+        couple: './src/assets/images/couple.png'
+    },
+
+    time: {
+        marriage: {
+            year: '2025',
+            month: 'Oktober',
+            date: '1',
+            day: 'Rabu',
+            hours: {
+                start: '11.00',
+                finish: 'Selesai'
+            }
+        },
+        reception: {
+            year: '2025',
+            month: 'Oktober',
+            date: '2',
+            day: 'Kamis',
+            hours: {
+                start: '08.00',
+                finish: 'Selesai'
+            }
+        },
+        address: 'Kp. Nyoman 2, RT 005/ RW 011, Desa.Cibatu tiga, Kec.Cariu, Kab.Bogor, Jawa barat (16840)'
+    },
+
+    link: {
+        calendar: 'https://calendar.app.google/exZ4vxFFAdarQnzT6',
+        map: 'https://goo.gl/maps/uMmtf7WoFu7uCsDm7',
+    },
+
+    galeri: [
+        {
+            id: 1,
+            image: './src/assets/images/1.png'
+        },
+        {
+            id: 2,
+            image: './src/assets/images/2.png'
+        },
+        {
+            id: 3,
+            image: './src/assets/images/3.png'
+        },
+        {
+            id: 4,
+            image: './src/assets/images/4.png'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/5.png'
+        }
+    ],
+
+    bank: [
+        {
+            id: 1,
+            name: 'Lorem Ipsum',
+            icon: './src/assets/images/bca.png',
+            rekening: '12345678'
+        },
+        {
+            id: 2,
+            name: 'Ipsum Lorem',
+            icon: './src/assets/images/bri.png',
+            rekening: '12345678'
+        },
+    ],
+
+    audio: './src/assets/audio/wedding.mp3',
+
+    api: 'https://script.google.com/macros/s/AKfycbyydz6N4p2VWUG8zsXeURv6ap9RP8a4eC3x6N3x6qTDjMVr1cIBz9S0NsHw2rWvBOSXGg/exec',
+
+    navbar: [
+        {
+            id: 1,
+            teks: 'Home',
+            icon: 'bx bxs-home-heart',
+            path: '#home',
+        },
+        {
+            id: 2,
+            teks: 'Mempelai',
+            icon: 'bx bxs-group',
+            path: '#bride',
+        },
+        {
+            id: 3,
+            teks: 'Tanggal',
+            icon: 'bx bxs-calendar-check',
+            path: '#time',
+        },
+        {
+            id: 4,
+            teks: 'Galeri',
+            icon: 'bx bxs-photo-album',
+            path: '#galeri',
+        },
+        {
+            id: 5,
+            teks: 'Ucapan',
+            icon: 'bx bxs-message-rounded-dots',
+            path: '#wishas',
+        },
+    ],
+}
